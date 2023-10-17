b1f71f51d22ceb2814877bfb0f20b27a7f09bf2c
Clearsu <clearwater1219@gmail.com>
2023-10-17 11:21:22 +0900

    [Feat] routing bw home and login page #1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { React } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import Home from './pages/Home';
 
 export default function App() {
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,17 +1,30 @@
 import { React } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/LoginForm.scss';
 
 export default function LoginForm() {
+  const navigate = useNavigate();
+
+  function loginHandler(e) {
+    e.preventDefault();
+    navigate('/home');
+  }
+
   return (
     <form className="login-form">
-      <div className="input-group">
-        <label>ID</label>
-        <input type="text" placeholder="아이디를 입력하세요." />
+      <div className="inputs-wrapper">
+        <div className="input-group">
+          <label>ID</label>
+          <input type="text" placeholder="아이디를 입력하세요." />
+        </div>
+        <div className="input-group">
+          <label>PW</label>
+          <input type="password" placeholder="비밀번호를 입력하세요." />
+        </div>
       </div>
-      <div className="input-group">
-        <label>PW</label>
-        <input type="password" placeholder="비밀번호를 입력하세요." />
-      </div>
+      <button type="submit" className="login-button" onClick={loginHandler}>
+        로그인
+      </button>
     </form>
   );
 }
