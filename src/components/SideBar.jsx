cf1a0540d4a80572fca74ba4161825c09466f6ed
Clearsu <clearwater1219@gmail.com>
2023-10-18 16:00:30 +0900

    [Feat] show sidebar page links by auth #1

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,15 +1,14 @@
 import { React } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
+import { useNavigate, Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import userState from '../userState';
 import '../styles/SideBar.scss';
 
 export default function SideBar() {
   const navigate = useNavigate();
-  const setRecoilStateValue = useSetRecoilState(userState);
+  const [recoilStateValue, setRecoilStateValue] = useRecoilState(userState);
 
   function logoutHandler() {
-    localStorage.removeItem('user');
     setRecoilStateValue({
       isLoggedIn: false,
       auth: '',
@@ -17,11 +16,18 @@ export default function SideBar() {
     navigate('/login');
   }
 
+  const auth = recoilStateValue.auth;
+
   return (
     <div className="side-bar">
       <button onClick={logoutHandler}>로그아웃</button>
-      <a href="/my-page">My page</a>
-      <a href="/setting">Setting</a>
+      {auth === 'manager' && <Link to="/manage">Manage</Link>}
+      {auth === 'admin' ? (
+        <Link to="/admin">Admin</Link>
+      ) : (
+        <Link to="/my-page">My page</Link>
+      )}
+      <Link to="/setting">Setting</Link>
     </div>
   );
 }
