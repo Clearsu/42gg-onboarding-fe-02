1a808227d2076f257ecbb844018c15ba45395fd5
Clearsu <clearwater1219@gmail.com>
2023-10-18 10:29:48 +0900

    [Feat] Home #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,33 @@
-import { React } from 'react';
+import { React, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import userState from '../userState';
+// import NavBar from '../components/NavBar';
+// import SideBar from '../components/SideBar';
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return <p>this is home</p>;
+  const navigate = useNavigate();
+  const userStateValue = useRecoilValue(userState);
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+
+    if (!user || !userStateValue.isLoggedIn) {
+      navigate('/login');
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <h1>this is home</h1>
+    </Layout>
+    // <div>
+    //   <NavBar />
+    //   <div className="sidebar-homecontent-wrapper">
+    //     <SideBar />
+    //     <p>this is home</p>
+    //   </div>
+    // </div>
+  );
 }
