701fca518ffd483a82b1d1f2101f337c38cee2f5
Clearsu <clearwater1219@gmail.com>
2023-10-18 10:28:51 +0900

    [Test] dummy userInfo #1

---
+++ src/userInfo.js
@@ -0,0 +1,19 @@
+const userInfo = [
+  {
+    id: 'jincpark',
+    pw: '1q2w3e4r',
+    auth: 'normal',
+  },
+  {
+    id: 'banana',
+    pw: 'bananaisyellow',
+    auth: 'manager',
+  },
+  {
+    id: 'apple',
+    pw: 'appleisred',
+    auth: 'admin',
+  },
+];
+
+export default userInfo;
