1f808824eb1adda355d30a441fc1dea51b9c3d94
Clearsu <clearwater1219@gmail.com>
2023-10-18 16:01:33 +0900

    [Refactor] Home #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,10 @@
-import { React, useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
-import { useNavigate } from 'react-router-dom';
-import userState from '../userState';
-// import NavBar from '../components/NavBar';
-// import SideBar from '../components/SideBar';
+import { React } from 'react';
 import Layout from '../components/Layout';
 
 export default function Home() {
-  const navigate = useNavigate();
-  const userStateValue = useRecoilValue(userState);
-
-  useEffect(() => {
-    const user = localStorage.getItem('user');
-
-    if (!user || !userStateValue.isLoggedIn) {
-      navigate('/login');
-    }
-  }, []);
-
   return (
     <Layout>
       <h1>this is home</h1>
     </Layout>
-    // <div>
-    //   <NavBar />
-    //   <div className="sidebar-homecontent-wrapper">
-    //     <SideBar />
-    //     <p>this is home</p>
-    //   </div>
-    // </div>
   );
 }
