4116bb6f9520b186a675ae13f2fbb2d1b6575d31
Clearsu <clearwater1219@gmail.com>
2023-10-19 11:14:40 +0900

    [Feat] add more contents #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,22 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <h1>this is home</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
+      <h1>This is home.</h1>
     </Layout>
   );
 }
