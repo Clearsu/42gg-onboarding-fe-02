393fd50e1009c96042db5e68402d4ad29430eb50
Clearsu <clearwater1219@gmail.com>
2023-10-19 13:33:36 +0900

    [Feat] show username and auth on Home page #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,17 @@
 import { React } from 'react';
+import { useRecoilValue } from 'recoil';
+import userState from '../userState';
 import Layout from '../components/Layout';
 
 export default function Home() {
+  const userStateValue = useRecoilValue(userState);
+
   return (
     <Layout>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
-      <h1>This is home.</h1>
+      <h1>
+        Welcome {userStateValue.username}
+        {userStateValue.auth !== 'normal' && ', ' + userStateValue.auth}!
+      </h1>
     </Layout>
   );
 }
